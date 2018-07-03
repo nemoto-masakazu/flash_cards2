580da4d214160b4699275063a261cfa1e9e828e1
Masakazu Nemoto <nemoto@fundely.co.jp>
2018-07-03 17:47:41 +0900

    changed a part of role and sever on /config/deploy/production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :app, %w{18.179.163.165}
+role :web, %w{18.179.163.165}
+role :db,  %w{18.179.163.165}
 
 
 # Extended Server Syntax
@@ -15,7 +15,7 @@ role :db,  %w{deploy@example.com}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server '18.179.163.165', user: 'flash', roles: %w{web app}, my_property: :my_value
 
 
 # Custom SSH Options
