98617402bd660a274aa4bed6db1aad7a4f311bfb
Masakazu Nemoto <nemoto@fundely.co.jp>
2018-07-06 15:25:17 +0900

    changed IP to localhost in database.yml and deploy/production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -15,7 +15,7 @@ role :db,  %w{18.179.163.165}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server '18.179.163.165', user: 'flash', roles: %w{web app db}, my_property: :my_value
+#server '18.179.163.165', user: 'flash', roles: %w{web app db}, my_property: :my_value
 
 
 # Custom SSH Options
